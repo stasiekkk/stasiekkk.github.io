157df640e433b98d7602ecbc318ffc020fc418c4
Stasiek <the.orginal.stasgaming@gmail.com>
2022-09-04 20:19:33 +0200

    up

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let counter = 0;
+function darkmode() {
+    var icon = $('#icon')
+    if (counter == 0) {
+        icon.removeClass('bi').removeClass('bi-moon-fill').addClass('bi-brightness-high-fill').css('color', 'white')
+        counter++;
+    } else {
+        icon.removeClass('bi').removeClass('bi-brightness-high-fill').addClass('bi-moon-fill').css('color', 'black')
+        counter = 0;
+    }
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var options = {
+        strings: ["Welcome"],
+        typeSpeed: 60,
+        backSpeed: 20,
+        backDelay: 500,
+        loop: false,
+        autoInsertCss: true,
+        smartBackspace: true
+    }
+    var typed = new Typed("#welcome", options);
+});
